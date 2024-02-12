ec947c8cbfde9d95f6b22e1ac2af208c72f66ce1
abdur rahim <abdurphilly@gmail.com>
2024-02-11 22:50:42 -0500

    main.py added for the purpose of Activity 5

---
+++ main.py
@@ -0,0 +1,6 @@
+def hello_user():
+    user_name = input("How do you say your beautiful name? : ")
+    user_age = int(input("How young are you? : "))
+    print(f"Hello {user_name}, glad to see you are {user_age} years young!")
+
+hello_user()
